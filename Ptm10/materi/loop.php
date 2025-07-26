c1cd193617e3b381b6da2f41030a9ce5823053d6
Jwaa.a_ <rajwaaangara123@gmail.com>
2025-07-26 14:10:21 +0700

    Create loop.php

---
+++ Ptm10/materi/loop.php
@@ -0,0 +1,20 @@
+<?php
+echo "<h1>Materi For</h1>";
+echo "for loop";
+echo "<br>";
+for ($i = 2; $i <= 10; $i += 2){
+    echo "angka: ".$i."<br>";
+}
+
+echo "<br>";
+echo "<br>";
+echo "while loop";
+echo "<br>";
+$j = 1;
+while ($j < 10){
+    echo "angka: ".$j."<br>";
+    $j += 2;
+}
+?>
+<br>
+<a href="index.php">kembali ke menu utama</a>
