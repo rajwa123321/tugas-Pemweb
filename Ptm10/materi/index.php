d4b8c944ae5f14585a5a8b32c0cebfcd767ce592
Jwaa.a_ <rajwaaangara123@gmail.com>
2025-07-26 14:09:46 +0700

    Create index.php

---
+++ Ptm10/materi/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pertemuan 10</title>
+</head>
+<body>
+    <h1>pertemuan 10</h1>
+    <a href="if.php">MATERI IF</a>
+    <br>
+    <a href="switch.php">SWITCH</a>
+    <br>
+    <a href="loop.php">MATERI LOOP</a>
+    <br>
+    <a href="function.php">MATERI FUNCTION</a>
+    <br>
+    <a href="array.php">MATERI ARRAY</a>
+    <br>
+    <a href="array_dan_looping.php">MATERI ARRAY DAN LOOPING</a>
+    <br>
+    <a href="../contoh/form_handling.html">MATERI FORM HANDLING</a>
+    <br>
+    <a href="../contoh/output.php">OUTPUT</a>
+</body>
+</html>
