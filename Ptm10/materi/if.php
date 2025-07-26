8fb773b3195dd2de43ed35a4cb78564e06716cf1
Jwaa.a_ <rajwaaangara123@gmail.com>
2025-07-26 14:08:51 +0700

    Create if.php

---
+++ Ptm10/materi/if.php
@@ -0,0 +1,15 @@
+<?php
+$umur = 60;
+echo "<h1>Kodingan If</h1>";
+if ($umur >= 18) {
+    echo "Anda sudah dewasa.";
+}
+elseif ($umur>=13) {
+    echo "anda masih remaja";
+}
+else{
+    echo "anda masih anak-anak";
+}
+?>
+<br>
+<a href="index.php">kembali ke menu utama</a>
